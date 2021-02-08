4c738503e83cfd8916a4cbc7b810c5cf068bfba3
Colin Huang <u8813345@users.noreply.github.com>
2021-02-08 11:56:17 +0800

    Create rpicom.sh

---
+++ rpicom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 這是我設定 我的debugger
+# HW: rpi2
+# OS: ubuntu server 20.04.2 32-bit
+#
+#
+# after install: 
+# defualt user name / password
+#            ubuntu / ubuntu
+#
+# defualt host name: ubuntu
+#
+#
+
